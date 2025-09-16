b231bfea5e3c36cd0c09bd688770d5743b600d1e
andylinee <andylin0404@gmail.com>
2025-09-16 18:21:44 +0800

    feat: Implement processing captured data

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..11);
 
     let mut attempts = 0;
-
     loop {
         println!("Please input a number: ");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Oops! Something goes wrong");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input valid number");
+                continue;
+            }
+        };
+        attempts += 1;
     }
 }
